9baefcd5db1583ec6d07090aed9829a0768f55af
brenohq <brenosc2@hotmail.com>
2021-10-12 23:50:39 -0300

    finishing delete and post delete re-arrangement

--- scripts/red-black-tree.py
+++ scripts/red-black-tree.py
@@ -56,9 +56,31 @@ class RedBlackTree():
         y.right = x
         x.parent = y
 
+    # find the node with the minimum key (leftmost node)
+    def minimum(self, node):
+        while node.left != self.TNULL:
+            node = node.left
+        return node
+
+    # find the node with the maximum key (rightmost node)
+    def maximum(self, node):
+        while node.right != self.TNULL:
+            node = node.right
+        return node
+
+    def __rb_transplant(self, u, v):
+        if u.parent == None:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
+
     def insert(self, key):
 
         node = Node(key)
+
         node.left = self.TNULL
         node.right = self.TNULL
 
@@ -95,9 +117,50 @@ class RedBlackTree():
         self.__fix_insert(node)
 
     def delete(self, key):
-        return True
+        # find the node containing key
+        z = self.TNULL
+        node = self.root
 
-    # fix the red-black tree
+        while node != self.TNULL:
+            if node.key == key:
+                z = node
+
+            if node.key <= key:
+                node = node.right
+            else:
+                node = node.left
+
+        if z == self.TNULL:
+            print("Couldn't find key in the tree")
+            return
+
+        y = z
+        y_original_color = y.color
+        if z.left == self.TNULL:
+            x = z.right
+            self.__rb_transplant(z, z.right)
+        elif (z.right == self.TNULL):
+            x = z.left
+            self.__rb_transplant(z, z.left)
+        else:
+            y = self.minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.parent == z:
+                x.parent = y
+            else:
+                self.__rb_transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+
+            self.__rb_transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if y_original_color == BLACK:
+            self.__fix_delete(x)
+
+    # fix the red-black tree modified when insertion occurs
     def __fix_insert(self, k):
         while k.parent.color == RED:
             if k.parent == k.parent.parent.right:
@@ -139,6 +202,65 @@ class RedBlackTree():
                 break
         self.root.color = BLACK
 
+    # fix the red-black tree modified when deletion occurs
+    def __fix_delete(self, x):
+        while x != self.root and x.color == BLACK:
+            if x == x.parent.left:
+                s = x.parent.right
+                if s.color == RED:
+                    # case x’s sibling S is red
+                    s.color = BLACK
+                    x.parent.color = RED
+                    self.left_rotate(x.parent)
+                    s = x.parent.right
+
+                if s.left.color == BLACK and s.right.color == BLACK:
+                    # case x’s sibling S is black, and both of S’s children are black
+                    s.color = RED
+                    x = x.parent
+                else:
+                    if s.right.color == BLACK:
+                        # case x’s sibling S is black, S’s left child is red, and S’s right child is black
+                        s.left.color = BLACK
+                        s.color = RED
+                        self.right_rotate(s)
+                        s = x.parent.right
+
+                    # case x’s sibling S is black, and S’s right child is red
+                    s.color = x.parent.color
+                    x.parent.color = BLACK
+                    s.right.color = BLACK
+                    self.left_rotate(x.parent)
+                    x = self.root
+            else:
+                s = x.parent.left
+                if s.color == RED:
+                    # case x’s sibling S is red
+                    s.color = BLACK
+                    x.parent.color = RED
+                    self.right_rotate(x.parent)
+                    s = x.parent.left
+
+                if s.left.color == BLACK and s.right.color == BLACK:
+                    # case x’s sibling S is black, and both of S’s children are black
+                    s.color = RED
+                    x = x.parent
+                else:
+                    if s.left.color == BLACK:
+                        # case x’s sibling S is black, S’s left child is red, and S’s right child is black
+                        s.right.color = BLACK
+                        s.color = RED
+                        self.left_rotate(s)
+                        s = x.parent.left
+
+                    # case x’s sibling S is black, and S’s right child is red
+                    s.color = x.parent.color
+                    x.parent.color = BLACK
+                    s.left.color = BLACK
+                    self.right_rotate(x.parent)
+                    x = self.root
+        x.color = BLACK
+
     def print_human_readable_tree(self):
         self.__print_helper(self.root, "", True)
 
@@ -168,12 +290,20 @@ if __name__ == "__main__":
     bst.insert(25)
     bst.insert(40)
     bst.insert(80)
+
     bst.delete(25)
+
     bst.insert(27)
     bst.insert(82)
     bst.insert(13)
+
     bst.delete(27)
+
     bst.insert(1)
     bst.insert(6)
+
+    bst.delete(17)
+
+    bst.insert(30)
 
     bst.print_human_readable_tree()
